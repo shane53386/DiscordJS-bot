b42a9eb02da933f893a3a333b07c5f23635591fb
Chutipon Sartjinpong <mchutipon@gmail.com>
2021-03-22 11:13:46 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,113 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const ical = require("node-ical");
+const client = new Discord.Client();
+
+let prefix = config.PREFIX;
+
+client.on("ready", () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", (message) => {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+
+	const commandBody = message.content.slice(prefix.length);
+	const args = commandBody.split(" ");
+	const command = args.shift().toLowerCase();
+
+	if (command === "hi") {
+		message.react("👋");
+		message.channel.send(`Hi! ${message.author.username}`);
+	} else if (command === "help") {
+		helpCommand(message);
+	} else if (command === "info") {
+		userInfo(message);
+	} else if (command === "hw") {
+		homework(message);
+	} else if (command === "setprefix") {
+		changePrefix(message, args[0]);
+	} else if (command === "play") {
+		play(message, args[0]);
+	} else {
+		message.channel.send(`Unknown command! Try ${prefix}help`);
+		helpCommand(message);
+	}
+});
+
+function helpCommand(message) {
+	let embed = new Discord.MessageEmbed();
+	embed.setColor("RANDOM");
+	embed.setTitle(`${message.guild.name}'s command`);
+	embed.addFields(
+		{ name: "Prefix", value: prefix, inline: false },
+		{ name: "User's info", value: "info", inline: false },
+		{ name: "Homework", value: "hw", inline: false },
+		{ name: "Set prefix", value: "setprefix", inline: false }
+	);
+	embed.setTimestamp();
+	message.channel.send(embed);
+	message.react("🆘");
+}
+
+function userInfo(message) {
+	let embed = new Discord.MessageEmbed();
+	embed.setColor("RANDOM");
+	embed.setThumbnail(message.author.avatarURL());
+	embed.setTitle(`${message.author.username}'s info`);
+	embed.setTimestamp();
+	message.channel.send(embed);
+	message.react("ℹ");
+}
+
+function homework(message) {
+	let embed = new Discord.MessageEmbed();
+	embed.setColor("RANDOM");
+	embed.setTitle("Homework");
+	const mcv =
+		"https://www.mycourseville.com/?q=courseville/ical/302300-JOFYYNVX2PIS3VQJ97WT";
+
+	ical.fromURL(mcv, {}, (err, data) => {
+		var work = "";
+		let date = Date.now();
+		for (let k in data) {
+			const event = data[k];
+			if (event.type == "VEVENT" && event.end > date) {
+				work += `• ${event.summary.slice(
+					16
+				)}\tDue to ${event.end.toLocaleTimeString(
+					"th-TH"
+				)} ${event.end.toLocaleDateString("th-TH")}\n`;
+			}
+		}
+		message.channel.send(work == "" ? "Nothing" : work);
+	});
+	embed.setTimestamp();
+	message.channel.send(embed);
+	message.react("📚");
+}
+
+function changePrefix(message, prefix) {
+	this.prefix = prefix;
+	message.channel.send(`Set new prefix to ${this.prefix}`);
+	message.react("👍");
+}
+
+function play(message, video) {
+	if (!message.member.voice.channel) {
+		console.log("Please connect to voice channel");
+		return;
+	}
+	const connection = message.member.voice.channel.join();
+	const dispatcher = connection.play(video);
+
+	dispatcher.on("start", () => {
+		console.log(`${video} is now playing!`);
+	});
+	dispatcher.on("finish", () => {
+		console.log(`${video} has finished playing!`);
+	});
+	dispatcher.on("error", console.error);
+}
+client.login(config.BOT_TOKEN);
