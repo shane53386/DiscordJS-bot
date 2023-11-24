5dad7857269eda60020a3ef1eab2a55c14b67098
shane53386 <6231314621@student.chula.ac.th>
2023-11-24 17:33:24 +0700

    rm cached

--- openai.config.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const { Configuration, OpenAIApi } = require("openai");
-
-const configuration = new Configuration({
-	apiKey: process.env.OPENAI_KEY,
-	organization: process.env.OPENAI_ORG,
-});
-const openai = new OpenAIApi(configuration);
-module.exports = { openai };
